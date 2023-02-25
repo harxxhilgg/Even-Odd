c54bc2a18e0148c82cf023ab4f38a5e3321812d5
Harshil Patel <120940967+harxxhilgg@users.noreply.github.com>
2023-02-26 00:26:51 +0530

    input output control

--- app.js
+++ app.js
@@ -2,11 +2,5 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler() {
-console.log("input is: " + txtInput);
-outputDiv.innerText = txtInput;
-}
-
-
 
 btnTranslate.addEventListener("click", clickHandler);
