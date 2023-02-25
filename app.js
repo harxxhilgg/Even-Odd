c95990dbfdd47fd4963091eff23835c1765eb5a5
Harshil Patel <120940967+harxxhilgg@users.noreply.github.com>
2023-02-25 23:57:50 +0530

    input out processing done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
-console.log(txtInput);
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("Clicked!");
-    console.log("input", txtInput.value);
-})
+
+function clickHandler() {
+    outputDiv.innerText = "ajsjsjsjjs.";
+}
+
+btnTranslate.addEventListener("click", clickHandler);
