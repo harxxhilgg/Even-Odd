4a633fb6db84c3dc345f9f40bf7bd17f33707c38
Harshil Patel <120940967+harxxhilgg@users.noreply.github.com>
2023-02-26 14:03:14 +0530

    Even Or Odd checker Done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
-
-
-btnTranslate.addEventListener("click", clickHandler);
+function checkNumber() {
+	var number = document.getElementById("numberInput").value;
+	if (number % 2 == 0) {
+		document.getElementById("result").innerHTML = number + " is even";
+	} else {
+		document.getElementById("result").innerHTML = number + " is odd";
+	}
+}
