0b828147f15cf41d49ffc8a55a951f7c38608b2d
Harshil Patel <120940967+harxxhilgg@users.noreply.github.com>
2023-04-12 22:53:44 +0530

    updated input properties

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-function checkNumber() {
-	var number = document.getElementById("numberInput").value;
-	if (number % 2 == 0) {
-		document.getElementById("result").innerHTML = number + " is even";
-	} 
-	else {
-		document.getElementById("result").innerHTML = number + " is odd";
+const numberInput = document.getElementById('numberInput');
+const checkBtn = document.getElementById('checkBtn');
+
+checkBtn.addEventListener("click", () => {
+	if (numberInput.value.length == 0) {
+		document.getElementById('result').innerHTML = "invalid or null input!";
 	}
-}
+	else {
+		if (numberInput.value <= 0 ) {
+			document.getElementById('result').innerHTML = "input can't be negetive or zero.";
+		}
+		else {
+			if(numberInput.value % 2 == 0) {
+				document.getElementById('result').innerHTML =  numberInput.value + " is even.";
+			}
+			else {
+				document.getElementById('result').innerHTML = numberInput.value + " is odd.";
+			}
+		}
+	}
+})
